590218654aed6001a2fcc3e3b5f39801263eaa74
Kethillyn <naykethillyn@gmail.com>
2024-08-09 18:05:50 -0300

    Mudando o caminho das fotos, personalização do admin e concertando barra de busca

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     OPCOES_CATEGORIA = [
@@ -11,8 +12,10 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices = OPCOES_CATEGORIA, default = ""  )
+    publicada = models.BooleanField(default=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=150, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    data = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
          return f"Fotografia [Nome = {self.nome}]"
